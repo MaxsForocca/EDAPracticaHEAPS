ca1ff41c9dace4c0b18cf71b055c46a23c7874cb
MaxsForocca <mforocca@unsa.edu.pe>
2023-06-17 11:12:12 -0500

    metodo insertar en TDA_heap

--- Heaps_Queue/TDA_heap.java
+++ Heaps_Queue/TDA_heap.java
@@ -3,31 +3,54 @@ import java.util.ArrayList;
 
 public class TDA_heap<T extends Comparable<T>>{
     private Node<T> root;
-    private ArrayList<T> lista; 
+    private ArrayList<Node<T>> lista; 
+
     public TDA_heap(){
         this.root = null;
-        this.lista = new ArrayList<T>();
+        this.lista = new ArrayList<Node<T>>();
     }
     public boolean isEmpty(){
         return this.root == null;
     }
     public void insertar(T dato){
+        int indice = lista.size();
+        Node<T> nuevo = new Node<T>(dato);
+        lista.add(new Node<T>(dato));
         if(isEmpty()){
-            this.root = new Node<T>(dato);
+            this.root = nuevo;
         }else{
-            // casos de insercion
+            // trabaja con el indice del padre
+            int indicePadre = indice/2;
+            // agregamos el nuevo nodo
+            if(lista.get(indicePadre).getIzquierda() == null)
+                lista.get(indicePadre).setIzquierda(nuevo);
+            else
+                lista.get(indicePadre).setDerecha(nuevo);
+            int comp = dato.compareTo(lista.get(indice/2).getData());
+            // mientras el indice del padre no sea menor a 0 (root no tiene padre)
+            // el nuevo dato sea mayor
+            while(indicePadre >= 0 && comp > 0){
+                insertar(lista.get(indice), indice);
+                indice = indicePadre;
+                indicePadre = indice/2;
+            }
         }
-        lista.add(dato);
+    }
+    private void insertar(Node<T> nod, int indice){
+        Node<T> padre = lista.get(indice/2);
+        T aux = padre.getData();
+        padre.setData(nod.getData());
+        nod.setData(aux);
     }
     public void eliminar(int indice){
         // casos de eliminacion
     }
-    /*
+    
     @Override 
     public String toString(){
-        String toStr;
-        for(T element : lista)
-            toStr = toStr + 
+        String toStr = "";
+        for(Node<T> n : lista)
+            toStr = toStr + n.toString();
+        return toStr;
     }
-     */
 }
